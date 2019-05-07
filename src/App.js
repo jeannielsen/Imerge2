be7d400a74a907ba7b0235b87f05f3a5d87427fa
Nielsen <jeanielsen@comcast.net>
2019-05-07 13:47:42 -0700

    changed page names to lowercase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Agencies from "./pages/Agencies";
-import Maps from "./pages/Maps";
+import Agencies from "./pages/agencies";
+import Maps from "./pages/maps";
 import Navbar from "./components/Navbar";
-import News from "./pages/News";
-import Translator from "./pages/Translator";
+import News from "./pages/news";
+import Translator from "./pages/translator";
 import ReactDOM from "react-dom";
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
@@ -37,14 +37,13 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/Agencies" component={Agencies} />
-          <Route exact path="/Maps" component={Maps} />
-          <Route exact path="/News" component={News} />
-          <Route exact path="/Translator" component={Translator} />
-
-        </Switch>
+        {/* <Switch> */}
+          <Route exact path={process.env.PUBLIC_URL + "/"} component={Home} />
+          <Route exact path={process.env.PUBLIC_URL + "/agencies"} component={Agencies} />
+          <Route exact path={process.env.PUBLIC_URL + "/maps"} component={Maps} />
+          <Route exact path={process.env.PUBLIC_URL + "/news"} component={News} />
+          <Route exact path={process.env.PUBLIC_URL + "/translator"} component={Translator} />
+        {/* </Switch> */}
       </div>
     </Router>
   );

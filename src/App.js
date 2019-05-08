a97a837255ca087eb12cbb069e99e90e2117c6b0
Nielsen <jeanielsen@comcast.net>
2019-05-08 10:22:11 -0700

    troubleshooting deployment issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { render } from 'react-dom'
-import { hashHistory, Router, Route, IndexRoute } from "react-router";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Agencies from "./pages/agencies";
 import Maps from "./pages/maps";
@@ -11,25 +10,25 @@ import Translator from "./pages/translator";
 import { useTranslation } from 'react-i18next';
 import "./i18n"
 
+
 function App() {
   const { t } = useTranslation();
 
   return (
     // Router component initialized to virtual DOM
-    <Router history = {hashHistory}>
-
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <div>
         <Navbar />
-       
+        <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/agencies" component={Agencies}/>
           <Route exact path="/maps" component={Maps}/>
           <Route exact path="/news" component={News}/>
           <Route exact path="/translator" component={Translator}/>
-     
+        </Switch>
+
       </div>
-    </Router>
-   
+    </HashRouter>
   );
 }
 
